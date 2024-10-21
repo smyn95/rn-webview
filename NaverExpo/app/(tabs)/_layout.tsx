09aacb0325825cac58ae431f7538f8e38015f27e
smyn95 <tlsalduszz@gmail.com>
2024-10-21 20:56:36 +0900

    사용자는 홈 화면을 볼 수 있다.
    - 네이버 홈 화면을 webView로 노출

--- NaverExpo/app/(tabs)/_layout.tsx
+++ NaverExpo/app/(tabs)/_layout.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+const HomeIcon = ({ focused, color }: { focused: boolean; color: string }) => {
+  const iconName = focused ? 'home' : 'home-outline';
+  return <MaterialCommunityIcons name={iconName} size={26} color={color} />;
+};
+
+const ShoppingIcon = ({
+  focused,
+  color,
+}: {
+  focused: boolean;
+  color: string;
+}) => {
+  const iconName = focused ? 'shopping' : 'shopping-outline';
+  return <MaterialCommunityIcons name={iconName} size={26} color={color} />;
+};
+
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="shopping" />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'black',
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'white',
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{ tabBarLabel: '홈', tabBarIcon: HomeIcon }}
+      />
+      <Tabs.Screen
+        name="shopping"
+        options={{ tabBarLabel: '쇼핑', tabBarIcon: ShoppingIcon }}
+      />
     </Tabs>
   );
 }
--- NaverExpo/app/(tabs)/index.tsx
+++ NaverExpo/app/(tabs)/index.tsx
@@ -1,8 +1,40 @@
+import { router } from 'expo-router';
 import React from 'react';
-import { Text } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  safeArea: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+});
 
 const HomeScreen = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <WebView
+        source={{ uri: 'https://m.naver.com' }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={request => {
+          if (
+            request.url.startsWith('https://m.naver.com') ||
+            request.mainDocumentURL?.startsWith('https://m.naver.com')
+          ) {
+            return true;
+          }
+
+          if (request.url != null && request.url.startsWith('https://')) {
+            router.navigate({
+              pathname: 'browser',
+            });
+          }
+          return true;
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
